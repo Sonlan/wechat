99178add9d63ea24fbe126183a0ca47a29fd8efa
Sonlan <1147649695@qq.com>
2016-07-01 16:21:20 +0800

    删除了不必要的冗余代码

--- src/com/song/web/entry.java
+++ src/com/song/web/entry.java
@@ -67,33 +67,4 @@ public class entry {
 	        }
 		}
 	}
-/*	@RequestMapping(value="/index")
-	public void index1(HttpServletRequest request,HttpServletResponse response) throws IOException{
-		request.setCharacterEncoding("utf-8");
-        response.setContentType("text/xml;charset=utf-8");
-        try {
-            Map<String, String> map = MessageUtil.xmlToMap(request);
-            String toUserName = map.get("ToUserName");
-            String fromUserName = map.get("FromUserName");
-            String msgType = map.get("MsgType");
-            String content = map.get("Content");
-            
-            String message = null;
-            if ("text".equals(msgType)) {                // 对文本消息进行处理
-                TextMessage text = new TextMessage();
-                text.setFromUserName(toUserName);         // 发送和回复是反向的
-                text.setToUserName(fromUserName);
-                text.setMsgType("text");
-                text.setCreateTime(new Date().getTime());
-                text.setContent("你发送的消息是：" + content);
-                message = MessageUtil.textMessageToXML(text);
-                System.out.println(message);            
-            }
-            response.getWriter().print(message);                            // 将回应发送给微信服务器
-        } catch (Exception e) {
-            e.printStackTrace();
-        }finally{
-        	response.getWriter().close();
-        }
-    }*/
 }
